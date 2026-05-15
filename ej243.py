2e865bb28aac28bd05e96f0210eaeaa4b23882b8
Juan Pablo <juanpablo3705@gmail.com>
2026-05-15 18:52:16 -0400

    terminado ok

--- ej243.py
+++ ej243.py
@@ -1,1 +1,19 @@
-print("hola")
+while True:
+    try:
+        cantidad_pasajes = int(input("¿Cuántos pasajes deseas vender?: "))
+        break
+    except ValueError:
+        print("Error. Ingrese un valor numérico entero.")
+
+precio_total = 0
+
+for contador in range(cantidad_pasajes):
+    while True:
+        try:
+            precio = int(input(f"Ingresa el precio del pasaje {contador+1}: "))
+            precio_total = precio_total + precio
+            break
+        except ValueError:
+            print("Error. Ingrese un valor numérico entero.")
+
+print(f"El precio total de los {cantidad_pasajes} pasajes es: ${precio_total}")
