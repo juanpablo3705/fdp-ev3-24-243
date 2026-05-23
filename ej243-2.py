24b19eb4771f7fb08c7ba58d975156a895080a0a
Juan Pablo <juanpablo3705@gmail.com>
2026-05-22 21:40:19 -0400

    segundo intento, terminado ok

---
+++ ej243-2.py
@@ -0,0 +1,27 @@
+while True:
+    try:
+        cantidad_pasajes = int(input("Ingrese la cantidad de pasajes a vender: "))
+        if cantidad_pasajes > 0:
+            break
+        else:
+            print("Error. Ingrese una cantidad de pasajes positiva.")
+    except ValueError:
+        print("Error. Ingrese números, no letras.")
+
+total_ingresos = 0
+
+for index in range(cantidad_pasajes):
+
+    while True:
+        try:
+            precio = int(input(f"Ingresa el precio del pasaje número {index + 1}: "))
+            if precio > 0:
+                break
+            else:
+                print("Error. El precio debe ser mayor que cero.")
+        except ValueError:
+            print("Error. Ingrese números, no letras.")
+
+    total_ingresos = total_ingresos + precio
+
+print(f"El total de ingresos por la venta de los pasajes es: ${total_ingresos}.")
